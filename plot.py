dcc8db32105e7f59962c0d11639bea9b55cf524d
bestestest kitteh <236551@edu.p.lodz.pl>
2023-05-22 19:12:50 +0200

    fix scaling

--- plot.py
+++ plot.py
@@ -8,11 +8,19 @@ from PIL import Image
 import prediction as p
 
 fn = sys.argv[1]
-a = p.predict(fn)
+result = p.predict(fn)
 img = np.asarray(Image.open(fn).resize((320, 320)))
-imgplot = plt.imshow(img)
-for e in a:
-    plt.plot([x + 10 for x in e["X"]], [y + 10 for y in e["Y"]], marker="o")
+
+plt.imshow(img)
+
+
+def fix(xs):
+    s = 3.2 / 3.0
+    return [x * s for x in xs]
+
+
+for e in result:
+    plt.plot(fix(e["X"]), fix(e["Y"]), marker="o")
 axes = plt.gca()
 axes.set_xlim([0, 320])
 axes.set_ylim([0, 320])
