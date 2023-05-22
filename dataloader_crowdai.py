ecd173cf8f9df2ef3a3e31104c556a343559b965
bestestest kitteh <236551@edu.p.lodz.pl>
2023-05-22 13:53:44 +0200

    uncleaned code >:3

--- dataloader_crowdai.py
+++ dataloader_crowdai.py
@@ -1,59 +1,43 @@
-import numpy as np
 import random
+
+import numpy as np
+import torch
+from pycocotools.coco import COCO
 from skimage import io
 from skimage.transform import resize
-import torch
 from torch.utils.data import Dataset
-from pycocotools.coco import COCO
 
 
 class CrowdAI(Dataset):
     """CrowdAI dataset"""
 
-    def __init__(self, images_directory, annotations_path):
-
+    def __init__(self, images_directory):
         self.IMAGES_DIRECTORY = images_directory
-        self.ANNOTATIONS_PATH = annotations_path
 
-        self.coco = COCO(self.ANNOTATIONS_PATH)
-
-        self.image_ids = self.coco.getImgIds(catIds=self.coco.getCatIds())
-
-        self.len = len(self.image_ids)
+        self.len = len([0])
 
         self.window_size = 320
         self.max_points = 256
 
-
     def loadSample(self, idx):
-
-        idx = self.image_ids[idx]
-
-        img = self.coco.loadImgs(idx)[0]
-        image_path = self.IMAGES_DIRECTORY + img['file_name']
-
-        image = io.imread(image_path)
-        image = resize(image, (self.window_size, self.window_size, 3), anti_aliasing=True, preserve_range=True)
-
-        annotation_ids = self.coco.getAnnIds(imgIds=img['id'])
-        annotations = self.coco.loadAnns(annotation_ids)
-        random.shuffle(annotations)
+        image = io.imread(str(idx) + ".png")
+        image = resize(
+            image,
+            (self.window_size, self.window_size, 3),
+            anti_aliasing=True,
+            preserve_range=True,
+        )
 
         image_idx = torch.tensor([idx])
         image = torch.from_numpy(image)
-        image = image.permute(2,0,1) / 255.0
+        image = image.permute(2, 0, 1) / 255.0
 
-        sample = {'image': image, 'image_idx': image_idx}
+        sample = {"image": image, "image_idx": image_idx}
         return sample
-
 
     def __len__(self):
         return self.len
 
-
     def __getitem__(self, idx):
-
         sample = self.loadSample(idx)
         return sample
-
-
--- prediction.py
+++ prediction.py
@@ -1,25 +1,24 @@
 import json
 import time
+
 import numpy as np
+import torch
+from pycocotools.coco import COCO
+from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-import torch
-from torch.utils.data import DataLoader
-
-from models.matching import OptimalMatching
-from models.backbone import R2U_Net, NonMaxSuppression, DetectionBranch
-
 from dataloader_crowdai import CrowdAI
-from pycocotools.coco import COCO
+from models.backbone import DetectionBranch, NonMaxSuppression, R2U_Net
+from models.matching import OptimalMatching
 
 
 def bounding_box_from_points(points):
     points = np.array(points).flatten()
-    even_locations = np.arange(points.shape[0]/2) * 2
+    even_locations = np.arange(points.shape[0] / 2) * 2
     odd_locations = even_locations + 1
     X = np.take(points, even_locations.tolist())
     Y = np.take(points, odd_locations.tolist())
-    bbox = [X.min(), Y.min(), X.max()-X.min(), Y.max()-Y.min()]
+    bbox = [X.min(), Y.min(), X.max() - X.min(), Y.max() - Y.min()]
     bbox = [int(b) for b in bbox]
     return bbox
 
@@ -27,52 +26,62 @@ def bounding_box_from_points(points):
 def single_annotation(image_id, poly):
     _result = {}
     _result["image_id"] = int(image_id)
-    _result["category_id"] = 100 
+    _result["category_id"] = 100
     _result["score"] = 1
     _result["segmentation"] = poly
     _result["bbox"] = bounding_box_from_points(_result["segmentation"])
     return _result
 
 
-def prediction(batch_size, images_directory, annotations_path):
-
+def prediction(batch_size, images_directory):
     # Load network modules
     model = R2U_Net()
-    model = model.cuda()
     model = model.train()
 
     head_ver = DetectionBranch()
-    head_ver = head_ver.cuda()
     head_ver = head_ver.train()
 
     suppression = NonMaxSuppression()
-    suppression = suppression.cuda()
 
     matching = OptimalMatching()
-    matching = matching.cuda()
     matching = matching.train()
 
-    # NOTE: The modules are set to .train() mode during inference to make sure that the BatchNorm layers 
-    # rely on batch statistics rather than the mean and variance estimated during training. 
+    # NOTE: The modules are set to .train() mode during inference to make sure that the BatchNorm layers
+    # rely on batch statistics rather than the mean and variance estimated during training.
     # Experimentally, using batch stats makes the network perform better during inference.
 
     print("Loading pretrained model")
-    model.load_state_dict(torch.load("./trained_weights/polyworld_backbone"))
-    head_ver.load_state_dict(torch.load("./trained_weights/polyworld_seg_head"))
-    matching.load_state_dict(torch.load("./trained_weights/polyworld_matching"))
+    model.load_state_dict(
+        torch.load(
+            "./trained_weights/polyworld_backbone", map_location=torch.device("cpu")
+        )
+    )
+    head_ver.load_state_dict(
+        torch.load(
+            "./trained_weights/polyworld_seg_head", map_location=torch.device("cpu")
+        )
+    )
+    matching.load_state_dict(
+        torch.load(
+            "./trained_weights/polyworld_matching", map_location=torch.device("cpu")
+        )
+    )
 
     # Initiate the dataloader
-    CrowdAI_dataset = CrowdAI(images_directory=images_directory, annotations_path=annotations_path)
-    dataloader = DataLoader(CrowdAI_dataset, batch_size=batch_size, shuffle=False, num_workers=batch_size)
+    CrowdAI_dataset = CrowdAI(
+        images_directory=images_directory,
+    )
+    dataloader = DataLoader(
+        CrowdAI_dataset, batch_size=batch_size, shuffle=False, num_workers=batch_size
+    )
 
     train_iterator = tqdm(dataloader)
 
     speed = []
     predictions = []
     for i_batch, sample_batched in enumerate(train_iterator):
-
-        rgb = sample_batched['image'].cuda().float()
-        idx = sample_batched['image_idx']
+        rgb = sample_batched["image"].float()
+        idx = sample_batched["image_idx"]
 
         t0 = time.time()
 
@@ -81,10 +90,9 @@ def prediction(batch_size, images_directory, annotations_path):
 
         _, graph_pressed = suppression(occupancy_grid)
 
-        poly = matching.predict(rgb, features, graph_pressed) 
+        poly = matching.predict(rgb, features, graph_pressed)
 
         speed.append(time.time() - t0)
-
 
         for i, pp in enumerate(poly):
             for p in pp:
@@ -104,7 +112,8 @@ def prediction(batch_size, images_directory, annotations_path):
     fp.close()
 
 
-if __name__ == '__main__':
-    prediction(batch_size=6,
-            images_directory="/home/stefano/Workspace/data/mapping_challenge_dataset/raw/val/images/",
-            annotations_path="/home/stefano/Workspace/data/mapping_challenge_dataset/raw/val/annotation.json")
+if __name__ == "__main__":
+    prediction(
+        batch_size=6,
+        images_directory="/home/stefano/Workspace/data/mapping_challenge_dataset/raw/val/images/",
+    )
